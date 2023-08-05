19c15f2c27eea69663329e06594d40751da16f68
박규경 <64006112+ggpark1218@users.noreply.github.com>
2023-08-05 19:17:00 +0900

    ✨ JS

--- 20230803/007_function.js
+++ 20230803/007_function.js
@@ -7,3 +7,26 @@ add(1,2)
 function (x,y){
     return x-y
 }
+
+function stringToNumber(str){
+    let answer = parseInt(str.replaceAll(',',''))
+    return answer
+}
+
+str = '10,000,000,000'
+stringToNumber(str)
+
+function printAndSplit(str){
+
+}
+
+function sumAndMul(x,y){
+    let sum = x+y
+    let ans = sum**2
+    return ans
+}
+x=1, y=2
+sumAndMul(x,y)
+
+
+func
