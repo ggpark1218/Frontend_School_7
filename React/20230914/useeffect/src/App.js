f0152293c5128139ee4920a6f8ee71921f8ccebe
박규경 <64006112+ggpark1218@users.noreply.github.com>
2023-09-14 20:47:43 +0900

    ✨ styled-components

---
+++ React/20230914/useeffect/src/App.js
@@ -0,0 +1,37 @@
+import {useState, useEffect} from 'react'
+
+function Counter (){
+  const [count, setCount] = useState(0)
+  const handleCountUp = (e) => {
+    setCount(count + 1)
+  }
+
+  useEffect(()=>{
+    if (count%2==0){
+      alert("짝수입니다~!")
+    } else {
+      alert("홀수입니다~!")
+    }
+    return (()=>{
+      alert("컴포넌트가 바뀌기 직전입니다!")
+    })
+
+  },[count])
+  
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={handleCountUp}>Up!</button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
